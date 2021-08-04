95a5f59fe74bf679cc806b7b3033d642e8c452b7
M33p5t3r <abram.shoki@gmail.com>
2021-08-04 07:34:32 +0200

    Initial Commit
    Initial Commit for IMDB Search Movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'movies',
     pathMatch: 'full'
   },
+  {
+    path: 'movies',
+    loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesPageModule)
+  },
+  {
+    path: 'movie-details/:id',
+    loadChildren: () => import('./pages/movie-details/movie-details.module').then(m => m.MovieDetailsPageModule)
+  },
 ];
+
 
 @NgModule({
   imports: [
